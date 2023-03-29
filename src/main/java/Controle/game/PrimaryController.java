1c0cbc6412ad9f735b6ec7a39c287e87cc03d913
jeremyHzn <heizmann.jeremy@gmail.com>
2023-03-29 11:54:33 +0200

    module : ajout     requires transitive javafx.graphics;

--- src/main/java/Controle/game/PrimaryController.java
+++ src/main/java/Controle/game/PrimaryController.java
@@ -9,4 +9,5 @@ public class PrimaryController {
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+    
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module Controle.game {
+    requires transitive javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
 
     opens Controle.game to javafx.fxml;
+
     exports Controle.game;
 }
